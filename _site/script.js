e86ca43f6133294523b44768a1276f02d761caf0
TwilCynder <twilcynder@mail.com>
2025-04-23 03:09:56 +0200

    WIP request-sending framework

--- _site/script.js
+++ _site/script.js
@@ -1,3 +1,20 @@
-function test(){
-    console.log("TEST");
+import { sendRequest } from "./lib/tsh.js";
+
+function registerN(name, f){
+    window[name] = f;
 }
+
+/**
+ * 
+ * @param {Function} f 
+ */
+function register(f){
+    registerN(f.name, f)
+}
+
+registerN("keydown", (elt) => {
+    console.log(elt);
+    sendRequest("http://192.168.1.101:5000","scoreboard0-team1-scoreup")
+});
+
+$(".loading").hide();
